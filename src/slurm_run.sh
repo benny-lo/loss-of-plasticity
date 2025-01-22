a0142986c862399ac5ca3255fa367139a116f7bf
benny-lo <lbenedett@student.ethz.ch>
2025-01-22 20:39:30 +0100

    fixed sh file for slurm

--- src/slurm_run.sh
+++ src/slurm_run.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --account=dl
 #SBATCH --job-name=test        
-#SBATCH --output=test.txt    
-#SBATCH --error=test_error.txt     
+#SBATCH --output=slurm_out.txt    
+#SBATCH --error=slurm_error.txt     
 #SBATCH --time=1-00:00:00 
 
-python src/experiments.py --experiment_name "$1"
+python src/experiments.py $1
